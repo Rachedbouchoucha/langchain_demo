85041cbb077d05164c660e73f394a0e9404991fb
Rached <rached@MacBook-Pro-de-Rached.local>
2024-01-28 15:09:45 -0500

    adding new tool to the toolbox

--- agents_main.py
+++ agents_main.py
@@ -1,42 +1,66 @@
-from langchain.agents import create_sql_agent
-from langchain_community.utilities import SQLDatabase
-from langchain_openai import OpenAI
-from langchain.agents.agent_types import AgentType
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
-import pymysql
-
-pymysql.install_as_MySQLdb()
-
-from langchain_openai import ChatOpenAI
-from langchain_community.utilities import SQLDatabase
-from langchain.agents import create_sql_agent
-from langchain_community.agent_toolkits import SQLDatabaseToolkit
-from langchain.agents.agent_types import AgentType
-import openai
+import ast
+import re
 import os
 from dotenv import load_dotenv
+from langchain_community.vectorstores import FAISS
+from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain_community.utilities import SQLDatabase
+from langchain_community.agent_toolkits import create_sql_agent, SQLDatabaseToolkit
+from langchain.agents.agent_toolkits import create_retriever_tool
+from langchain.agents.agent_types import AgentType
+import pymysql
+pymysql.install_as_MySQLdb()
 
 load_dotenv()
 openai_api_key = os.getenv('openai_api_key')
+os.environ['OPENAI_API_KEY'] = openai_api_key
 conn_string = "sqlite:///Chinook.db"
 
 
+
+def query_as_list(db, query):
+    res = db.run(query)
+    res = [el for sub in ast.literal_eval(res) for el in sub if el]
+    res = [re.sub(r"\b\d+\b", "", string).strip() for string in res]
+    return list(set(res))
+
+
+def get_retreival_tool(db):
+    artists = query_as_list(db, "SELECT Name FROM Artist")
+    albums = query_as_list(db, "SELECT Title FROM Album")
+    vector_db = FAISS.from_texts(artists + albums, OpenAIEmbeddings())
+    retriever = vector_db.as_retriever(search_kwargs={"k": 5})
+    description = """Use to look up values to filter on. Input is an approximate spelling of the proper noun, output is \
+    valid proper nouns. Use the noun most similar to the search."""
+    retriever_tool = create_retriever_tool(
+        retriever,
+        name="search_proper_nouns",
+        description=description,
+    )
+
+    return retriever_tool
+
+
 def get_response(question):
-    # with open('./prompts/sql_query_generation_prompt.txt', 'r', encoding='utf-8') as file:
-    #     prompt_template = file.read()
+
+    with open('prompt.txt', 'r', encoding='utf-8') as file:
+        prompt = file.read()
 
     db = SQLDatabase.from_uri(conn_string)
 
+    retriever_tool = get_retreival_tool(db=db)
     agent_executor = create_sql_agent(
         llm=ChatOpenAI(temperature=0, openai_api_key=openai_api_key,
-                       model="gpt-3.5-turbo"),
+                       model="gpt-4"),
         toolkit=SQLDatabaseToolkit(db=db, llm=ChatOpenAI(temperature=0,
                                                          openai_api_key=openai_api_key,
                                                          model="gpt-3.5-turbo")),
         verbose=True,
         agent_type=AgentType.OPENAI_FUNCTIONS,
+        suffix=prompt,
+        extra_tools=[retriever_tool],
         handle_parsing_errors=True,
-        max_iterations=20
+        max_iterations=20,
     )
 
     ai_response = agent_executor.invoke(question)
@@ -44,6 +68,4 @@ def get_response(question):
     return ai_response['output']
 
 
-if __name__ == '__main__':
-    response = get_response('Find all albums for the artist AC/DC')
-    print(response)
+
